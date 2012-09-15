8064f93385b966f80cf5c20820f79ddbf67df2b0
alexfernandez <alejandro@moveinblue.com>
2012-09-15 20:08:05 +0200

    V8′s idle garbage collection is disabled.

--- bash/milliearth.sh
+++ bash/milliearth.sh
@@ -2,5 +2,5 @@
 # Alex 2012-09-14: start MilliEarth server
 
 cd /home/milliearth/deploy/
-supervisor ~milliearth/deploy/src/milliearth.js &>> log_milliearth
+supervisor –nouse-idle-notification ~milliearth/deploy/src/milliearth.js &>> log_milliearth
 
