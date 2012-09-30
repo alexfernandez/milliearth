4b7600a6a97c0bd7c812915623cc620ed86131f8
alexfernandez <alejandro@moveinblue.com>
2012-09-30 23:19:58 +0200

    Refined position.

--- src/world.js
+++ src/world.js
@@ -434,7 +434,7 @@ var gameWorld = function(id)
 			var sqrt = Math.sqrt(2 * distance * distance) / 2;
 			robot.start(
 				//new vector(distance + 2, -10, 0),
-				new vector(sqrt + 2, sqrt + 2, 0),
+				new vector(sqrt + 4, sqrt + 4, 0),
 				new vector(0, 1, 0));
 		}
 		return robot;
