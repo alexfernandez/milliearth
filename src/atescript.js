211a91663a45690c3d9e9ba82c09d47d034c0b93
alexfernandez <alejandro@moveinblue.com>
2012-10-02 23:56:51 +0200

    Preliminary version of atescript engine.

---
+++ src/atescript.js
@@ -0,0 +1,58 @@
+'use strict';
+/**
+ * MilliEarth atescript scripting engine.
+ * (C) 2012 Alex Fernández.
+ *
+ * This file is part of MilliEarth.
+ *
+ * MilliEarth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MilliEarth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MilliEarth.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * Requirements.
+ */
+var globalParams = require('./params.js').globalParams;
+var vector = require('./vector.js').vector;
+var util = require('./util.js');
+var parser = util.parser;
+var log = util.log;
+var trace = util.trace;
+var extend = util.extend;
+
+
+/**
+ * A scripting engine.
+ */
+function scriptingEngine(params)
+{
+	// self-reference
+	var self = this;
+
+	// attributes
+	self.file = params.file;
+	self.robot = params.robot;
+
+	function readScript(file)
+	{
+		fs.readFile('src/script/' + file, function(err, data) {
+			if (err)
+			{
+				log('Invalid script file ' + file);
+				return;
+			}
+		});
+	}
+}
+
